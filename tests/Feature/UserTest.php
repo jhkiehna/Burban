b6adcb9ae59a8f2a4032b635e766533be25e7c83
jhkiehna <jhkiehna@gmail.com>
2019-02-20 08:54:14 -0500

    added a user test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -8,13 +8,24 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
+    public function testAuthTokenIsAcceptedforAUser()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $user->api_token
+        ])->json('GET', '/user/logout');
+
+        $response->assertStatus(204);
+    }
+
     public function testReturnsUnauthorizedResponseWhenTokenIsWrong()
     {
         $user = factory(User::class)->create();
 
         $response = $this->withHeaders([
             'Authorization' => 'Bearer '. 'asfsafsadfasf'
-        ])->json('GET', '/deals/saved');
+    ])->json('GET', '/user/logout');
 
         $response->assertStatus(401);
     }
